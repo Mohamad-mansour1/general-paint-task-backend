2c5d6cf9fb2d9ad6b3ab999a63ec980689b98e9f
Mohamad-mansour1 <109984960+Mohamad-mansour1@users.noreply.github.com>
2023-10-22 13:21:14 +0300

    get API

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CountController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/average', [CountController::class, 'calculateAverage'])->name('average');
+Route::get('/productCount', [HomeController::class, 'productCount'])->name('productCount');
